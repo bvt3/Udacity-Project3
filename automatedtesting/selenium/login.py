4496783428dddcf26ccd23c95b02013216ee86a9
Ben <bvt3@ymail.com>
2023-08-04 23:10:07 +0800

    Test

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -1,16 +1,19 @@
 # #!/usr/bin/env python
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.service import Service
 
 
 # Start the browser and login with standard_user
 def login (user, password):
+    service = Service(executable_path=r'/home/devopsagent/.cache/selenium/chrome/linux64/115.0.5790.170/chrome')
     print ('Starting the browser...')
     # --uncomment when running in Azure DevOps.
-    # options = ChromeOptions()
-    # options.add_argument("--headless") 
+    options = ChromeOptions()
+    options.add_argument("--headless") 
     # driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome()
+    #driver = webdriver.Chrome()
+    driver = webdriver.Chrome(options=options, service=service)
     print ('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
 
