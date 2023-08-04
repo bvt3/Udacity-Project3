532ba354da016cfe2b57bf1e9c116e62ffe8ecee
Ben <bvt3@ymail.com>
2023-08-04 09:51:01 +0800

    Change appservice module objects

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -1,30 +1,30 @@
-# resource "azurerm_service_plan" "test" {
-resource "azurerm_app_service_plan" "test" {
+resource "azurerm_service_plan" "test" {
+#resource "azurerm_app_service_plan" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
   # os_type             = "Linux"
   os_type             = "Windows"
-  # sku_name            = "B1"
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
+  sku_name            = "F1"
+  # sku {
+  #   tier = "Free"
+  #   size = "F1"
+  # }
 }
 
-# resource "azurerm_linux_web_app" "test" {
-resource "azurerm_app_service" "test" {
+resource "azurerm_windows_web_app" "test" {
+# resource "azurerm_app_service" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  # service_plan_id     = azurerm_service_plan.test.id
-  app_service_plan_id = azurerm_app_service_plan.test.id
+  service_plan_id     = azurerm_service_plan.test.id
+  # app_service_plan_id = azurerm_app_service_plan.test.id
 
   app_settings = {
     "WEBSITE_RUN_FROM_PACKAGE" = 1
   }
 
-  # site_config {
-  #   always_on = false    
-  # }
+  site_config {
+    always_on = false    
+  }
 }
