57bdfec02de8a17314b597ed38838c7b8e541112
Ben <bvt3@ymail.com>
2023-08-05 00:39:36 +0800

    Test

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -1,23 +1,51 @@
-# #!/usr/bin/env python
+# # #!/usr/bin/env python
+# from selenium import webdriver
+# #from selenium.webdriver.chrome.options import Options as ChromeOptions
+# from selenium.webdriver.chrome.service import Service
+
+
+# # Start the browser and login with standard_user
+# def login (user, password):
+#     caps = {}
+#     caps['browserName'] = 'chrome'
+
+#     service = Service(executable_path="/home/devopsagent/app")
+#     print ('Starting the browser...')
+#     # --uncomment when running in Azure DevOps.
+#     options = webdriver.ChromeOptions()
+#     options.add_argument("--headless") 
+#     # driver = webdriver.Chrome(options=options)
+#     #driver = webdriver.Chrome()
+#     driver = webdriver.Chrome(service=service, options=options)
+#     print ('Browser started successfully. Navigating to the demo page to login.')
+#     driver.get('https://www.saucedemo.com/')
+#     service.stop()
+
+# login('standard_user', 'secret_sauce')
+
+# print('asdf')
+
 from selenium import webdriver
-#from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+from time import sleep
 
 
-# Start the browser and login with standard_user
-def login (user, password):
-    service = Service(executable_path="/home/devopsagent/app")
-    print ('Starting the browser...')
-    # --uncomment when running in Azure DevOps.
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless") 
-    # driver = webdriver.Chrome(options=options)
-    #driver = webdriver.Chrome()
-    driver = webdriver.Chrome(service=service, options=options)
-    print ('Browser started successfully. Navigating to the demo page to login.')
-    driver.get('https://www.saucedemo.com/')
-    service.stop()
+# Set options for not prompting DevTools information
+options = Options()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
-login('standard_user', 'secret_sauce')
+print("testing started")
+driver = webdriver.Chrome(options=options)
+driver.get("https://www.saucedemo.com/")
+sleep(3)
 
-print('asdf')
+# Get page title
+title = driver.title
+
+# Test if title is correct
+assert "Swag Labs" in title
+print("TEST 0: `title` test passed")
+
+# Close the driver
+driver.quit()
