d49dbf1049d615b8596c9c5037da10e8d282af01
Ben <bvt3@ymail.com>
2023-08-07 10:42:21 +0000

    Gave up AzureRMWebAppDeployment, reverting back

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -6,14 +6,14 @@ resource "azurerm_service_plan" "test" {
   sku_name            = "F1"
 }
 
-resource "azurerm_linux_web_app" "test" {
+resource "azurerm_windows_web_app" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
   service_plan_id     = azurerm_service_plan.test.id
 
   app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE" = 0
+    "WEBSITE_RUN_FROM_PACKAGE" = 1
   }
 
   site_config {
